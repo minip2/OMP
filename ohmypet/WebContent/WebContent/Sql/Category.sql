4d3f46bea02fffd8dd8800302f41fcdb33411b33
Inchijeong <cde0409@naver.com>
2017-08-29 20:22:45 +0900

    ㅁ

--- ohmypet/WebContent/WebContent/Sql/Category.sql
+++
@@ -1,7 +0,0 @@
-create table t97_category (
-    group_val number(6),
-    category_val number(6),
-    category_name varchar2(50),
-    constraint categoryPK primary key (group_val, category_val);
-);
-
